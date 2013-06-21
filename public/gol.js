bcafd9dd49dc7a7be70721ce8a891c9bc30d572f
Greg Dziemidowicz <greg@cogent.co>
2013-06-21 22:08:10 +1000

    initial commit

---
+++ public/gol.js
@@ -0,0 +1,128 @@
+var socket = io.connect('/');
+
+var golState = new Array(1);
+golState[0] = new Array(1);
+
+socket.on('news', function (data) {
+    golState = data.location;
+    drawBoard();
+});
+
+function PeekabooCtrl($scope) {
+    $scope.yourName = "Greg";
+}
+
+var kBoardWidth = 20;
+var kBoardHeight= 10;
+var kPieceWidth = 50;
+var kPieceHeight= 50;
+var kPixelWidth = 1 + (kBoardWidth * kPieceWidth);
+var kPixelHeight= 1 + (kBoardHeight * kPieceHeight);
+
+var gCanvasElement;
+var gDrawingContext;
+var gPattern;
+
+var gPieces;
+var gNumPieces;
+var gSelectedPieceIndex;
+var gSelectedPieceHasMoved;
+var gMoveCount;
+var gGameInProgress;
+
+function Cell(row, column) {
+    this.row = row;
+    this.column = column;
+}
+
+function getCursorPosition(e) {
+    /* returns Cell with .row and .column properties */
+    var x;
+    var y;
+    if (e.pageX != undefined && e.pageY != undefined) {
+        x = e.pageX;
+        y = e.pageY;
+    }
+    else {
+        x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+    x -= gCanvasElement.offsetLeft;
+    y -= gCanvasElement.offsetTop;
+    x = Math.min(x, kBoardWidth * kPieceWidth);
+    y = Math.min(y, kBoardHeight * kPieceHeight);
+    var cell = new Cell(Math.floor(y/kPieceHeight), Math.floor(x/kPieceWidth));
+    return cell;
+}
+
+function halmaOnClick(e) {
+    var cell = getCursorPosition(e);
+    socket.emit('locationUpdate', { location: cell });
+}
+
+function drawBoard() {
+
+    gDrawingContext.clearRect(0, 0, kPixelWidth, kPixelHeight);
+
+    gDrawingContext.beginPath();
+
+    /* vertical lines */
+    for (var x = 0; x <= kPixelWidth; x += kPieceWidth) {
+        gDrawingContext.moveTo(0.5 + x, 0);
+        gDrawingContext.lineTo(0.5 + x, kPixelHeight);
+    }
+
+    /* horizontal lines */
+    for (var y = 0; y <= kPixelHeight; y += kPieceHeight) {
+        gDrawingContext.moveTo(0, 0.5 + y);
+        gDrawingContext.lineTo(kPixelWidth, 0.5 +  y);
+    }
+
+    /* draw it! */
+    gDrawingContext.strokeStyle = "#ccc";
+    gDrawingContext.stroke();
+
+    var length = golState.length,
+        width = 0,
+        element = null;
+    for (var x = 0; x < length; x++) {
+        width = golState[x].length
+        for (var y = 0; y < width; y++) {
+            element =  golState[x][y];
+            if (element) {
+                drawPiece(x,y);
+            }
+        }
+   }
+}
+
+function drawPiece(column, row) {
+    var x = (column * kPieceWidth) + (kPieceWidth/2);
+    var y = (row * kPieceHeight) + (kPieceHeight/2);
+    var radius = (kPieceWidth/2) - (kPieceWidth/10);
+    gDrawingContext.beginPath();
+    gDrawingContext.arc(x, y, radius, 0, Math.PI*2, false);
+    gDrawingContext.closePath();
+    gDrawingContext.strokeStyle = "#000";
+    gDrawingContext.stroke();
+
+    gDrawingContext.fillStyle = "#000";
+    gDrawingContext.fill();
+}
+
+
+function initGame(canvasElement) {
+    if (!canvasElement) {
+        canvasElement = document.createElement("canvas");
+        canvasElement.id = "halma_canvas";
+        document.body.appendChild(canvasElement);
+    }
+    gCanvasElement = canvasElement;
+    gCanvasElement.width = kPixelWidth;
+    gCanvasElement.height = kPixelHeight;
+    gCanvasElement.addEventListener("click", halmaOnClick, false);
+    gDrawingContext = gCanvasElement.getContext("2d");
+    drawBoard();
+}
+
+initGame();
---
+++ web.js
@@ -0,0 +1,100 @@
+var cronJob = require('cron').CronJob;
+
+var app = require('express')()
+    , server = require('http').createServer(app)
+    , io = require('socket.io').listen(server)
+    , express = require("express");
+
+var emptyState = function() {
+    var state = new Array(20);
+    for (var i = 0; i < 20; i++) {
+        state[i] = new Array(10);
+    }
+    return state;
+}
+
+var countNeighbours = function(x,y) {
+    var count = 0;
+
+    if (x-1 > 0) {
+        if(y-1>0) {
+            if (stateGol[x-1][y-1] == true) count = count + 1;
+        }
+        if (stateGol[x-1][y] == true) count = count + 1;
+        if(y+1<10) {
+            if (stateGol[x-1][y+1] == true) count = count + 1;
+        }
+    }
+
+    if(y-1>0) {
+        if (stateGol[x][y-1] == true ) count = count + 1;
+    }
+    if(y+1<10) {
+        if (stateGol[x][y+1] == true ) count = count + 1;
+    }
+
+    if (x+1 < 20) {
+        if(y-1>0) {
+            if (stateGol[x+1][y-1] == true ) count = count + 1;
+        }
+        if (stateGol[x+1][y] == true ) count = count + 1;
+        if(y+1<10) {
+            if (stateGol[x+1][y+1] == true) count = count + 1;
+        }
+    }
+
+    return count;
+}
+
+var stateGol = emptyState();
+
+stateGol[1][2] = true;
+stateGol[1][1] = true;
+stateGol[2][2] = true;
+
+
+port = process.env.PORT || 8080;
+
+server.listen(port);
+
+app.use(express.static('public'));
+app.use(express.static('components'));
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.configure(function () {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+});
+
+new cronJob('*/3 * * * * *', function(){
+
+    var element = null;
+    var newstate = emptyState();
+    var count = 0;
+    for (var x = 0; x < 20; x++) {
+        for (var y = 0; y < 10; y++) {
+            count = countNeighbours(x,y);
+            element =  stateGol[x][y];
+            if (count == 3 || count == 2 && element) {
+                newstate[x][y] = true;
+            }
+        }
+    }
+
+    stateGol = newstate;
+
+    io.sockets.emit('news', { location: stateGol });
+
+}, null, true, "America/Los_Angeles");
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { location: stateGol });
+    socket.on('locationUpdate', function (data) {
+        stateGol[data.location.column][data.location.row] = true;
+        io.sockets.emit('news', { location: stateGol });
+    });
+
+});
